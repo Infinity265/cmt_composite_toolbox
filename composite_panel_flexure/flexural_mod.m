2e37a6cdb472cb7960880e045a1541ae3cd132c3
duiker0 <154715563+Infinity265@users.noreply.github.com>
2024-01-11 13:19:44 +0800

    Added functionality for determining EI

---
+++ composite_panel_flexure/flexural_mod.m
@@ -0,0 +1,12 @@
+function EI = flexural_mod()
+    % Returns composite panel flexural modulus (EI)
+    %
+    % Arguments:
+    %   ABD  :  Laminate ABD matrix
+    %
+    % Returns
+    %   A_star  :  In-plane stiffness matrix with restrained curvature
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
