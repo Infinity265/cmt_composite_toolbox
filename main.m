8d69aa83f3e33359e0076441f8a4bba87dc63a7d
Infinity265 <154715563+Infinity265@users.noreply.github.com>
2024-02-23 00:05:08 +0800

    Various updates in the hunt of bugs

--- main.m
+++ main.m
@@ -1,8 +1,10 @@
 %% Currently feasibility of UD layups 
-addpath 'C:\Users\matth\Documents\GitHub\cmt_composite_toolbox\classical_laminate_theory' %Cause I wrote the code bad
+%Add sub folders to path so that MATLAB can find the funcs in subfolders
+addpath(strcat(pwd, '\classical_laminate_theory'))
+clear
 
 %% Layup input files
-layup = 'test_layup_1'
+layup = 'test_layup_1';
 material_xlsx_file = "\classical_laminate_theory\CLT_input_ply_mat_data\gurit_data_sheets.xlsx";
 
 layup_xlsx_file = strcat("\classical_laminate_theory\CLT_input_layup_data\", layup, ".xlsx");
@@ -15,7 +17,7 @@ abd = inv(ABD);
 % The engineering constants of the laminate can be determined from the
 % abd-matrix as:
 
-E_x = -1 * (1/(abd(1,1)*t))   %% Do not foget you added a * -1 here!!
-G_xy= -1 * (1/(abd(6,6)*t)) 
+E_x = -1 * (1/(abd(1,1)*t));   %% Do not foget you added a * -1 here!!
+G_xy= -1 * (1/(abd(6,6)*t));
 
 
