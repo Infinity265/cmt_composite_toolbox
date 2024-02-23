7adfc1a73a13275d16b39e827365d123be7fbdfb
Infinity265 <154715563+Infinity265@users.noreply.github.com>
2024-02-23 20:32:27 +0800

    Updates to ABD_function

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 %% Currently feasibility of UD layups 
+
 %Add sub folders to path so that MATLAB can find the funcs in subfolders
 addpath(strcat(pwd, '\classical_laminate_theory'))
 clear
@@ -17,7 +18,7 @@ abd = inv(ABD);
 % The engineering constants of the laminate can be determined from the
 % abd-matrix as:
 
-E_x = -1 * (1/(abd(1,1)*t));   %% Do not foget you added a * -1 here!!
-G_xy= -1 * (1/(abd(6,6)*t));
+E_x = (1/(abd(1,1)*t));
+G_xy = (1/(abd(6,6)*t));
 
 
